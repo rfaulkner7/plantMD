dabdfc17bd4073a31d2b84f12fa09d08967bbb32
rfaulkner7 <rfaulkner7@gatech.edu>
2020-11-06 22:06:05 -0500

    stuff

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Results from './Pages/Results'
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  return (
+<h1> HI</h1>
+  );
+}
+
+export default App;
---
+++ src/Pages/Results.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "../App.css"
+
+export default function Results (props){
+    return(
+        <div class = "box">
+            <h1 class = "intro">Welcome!</h1>
+        </div>
+    );
+}
